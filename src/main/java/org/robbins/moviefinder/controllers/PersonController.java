9b08f2a3c3a9d348c961c4b84a8f5ded9e2895b9
Justin Robbins <justinhrobbins@gmail.com>
2022-02-09 13:10:04 -0700

    simple controller for watch providers

--- src/main/java/org/robbins/moviefinder/controllers/PersonController.java
+++ src/main/java/org/robbins/moviefinder/controllers/PersonController.java
@@ -2,7 +2,6 @@ package org.robbins.moviefinder.controllers;
 
 import java.util.List;
 
-import org.robbins.moviefinder.TmdbConfigProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,25 +15,20 @@ import info.movito.themoviedbapi.model.people.PersonCredits;
 @RestController
 @RequestMapping("person")
 public class PersonController {
-
-    final TmdbConfigProperties tmdbProperties;
-    final TmdbApi tmdbApi;;
+    final TmdbApi tmdbApi;
     
-    public PersonController(final TmdbConfigProperties tmdbProperties) {
-        this.tmdbProperties = tmdbProperties;
-        tmdbApi = new TmdbApi(tmdbProperties.getApikey());
+    public PersonController(final TmdbApi tmdbApi) {
+        this.tmdbApi = tmdbApi;
     }
 
     @GetMapping("/find")
-    public List<Person> findPerson(@RequestParam("name") final String personName) {
-        
+    public List<Person> findPerson(@RequestParam("name") final String personName) {        
         final List<Person> persons = tmdbApi.getSearch().searchPerson(personName, false, 0).getResults();
         return persons;
     }
 
     @GetMapping("/{personId}/movies")
     public PersonCredits getMoviesForPerson(@PathVariable("personId") final int personId) {
-        
         final PersonCredits credits = tmdbApi.getPeople().getCombinedPersonCredits(personId);
         return credits;
     }
