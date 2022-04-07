099d6c85191c4a329a202dbe989711fbdb89cb86
Justin Robbins <justinhrobbins@gmail.com>
2022-04-07 13:55:17 -0600

    Feature 20 (#21)
    * Add Actor Search Bar
    
    * remove commented code

--- src/ui/src/pages/ActorSearchPage.js
+++ src/ui/src/pages/ActorSearchPage.js
@@ -1,31 +1,53 @@
 import { React, useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { ActorSearchCard } from '../components/ActorSearchCard';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { ActorSearchResultsCard } from '../components/ActorSearchResultsCard';
 
 export const ActorSearchPage = () => {
   const [searchParams] = useSearchParams()
   const actorName = searchParams.get('actorName');
   const [actors, setActors] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(
     () => {
-      const fetchActors = async () => {
-        const response = await fetch(`http://localhost:8080/person/find?name=${actorName}`);
-        const data = await response.json();
-        setActors(data);
-      };
-
-      fetchActors();
+      fetchActors(actorName);
     }, []
   );
 
-  if (!actors || actors.length === 0) {
-    return <h1>Actors not found</h1>
-  }
+  const fetchActors = async (name) => {
+    if (name && name.length > 0) {
+      const response = await fetch(`http://localhost:8080/person/find?name=${name}`);
+      const data = await response.json();
+      setActors(data);
+    }
+  };
+
+  const handleSearchBarChange = (searchBarQuery) => {
+    if (searchBarQuery && searchBarQuery.length > 2) {
+      // navigate('?actorName=' + searchBarQuery);
+      fetchActors(searchBarQuery)
+    }
+  };
+
   return (
     <div className="ActorSearchPage">
-      <h2>Actors</h2>
-      {actors.map(actor => <ActorSearchCard key={actor.id} actor={actor} />)}
+      <label>Search</label>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => { handleSearchBarChange(e.target.value) }} />
+
+      {
+        ((actorName && actorName.length > 0) && (!actors || actors.length === 0)) &&
+        <h1>Actor not found</h1>
+      }
+
+      {
+        (!actors || actors.length > 0) &&
+        <h2>Actors</h2>
+      }
+
+      {actors.map(actor => <ActorSearchResultsCard key={actor.id} actor={actor} />)}
     </div>
   );
 }
