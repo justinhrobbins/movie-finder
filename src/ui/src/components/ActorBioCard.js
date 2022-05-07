21d9666c24ad9cae735226a4fd27790d28466e1d
Justin Robbins <justinhrobbins@gmail.com>
2022-05-07 15:07:41 -0600

    refactor actor bio into its own component

--- src/ui/src/components/ActorBioCard.js
+++ src/ui/src/components/ActorBioCard.js
@@ -12,34 +12,33 @@ export const ActorBioCard = ({ actor }) => {
     if (!actor) return null;
 
     return (
-        <div className="actor-detail-card-content-actor-bio-container">
-            <div className="actor-detail-card-content-container">
-                <span className="actor-detail-card-content-label">Birthday:</span> {actor.birthday}
+        <div className="ActorBioCard">
+            <div className="actor-bio-card-content-container">
+                <span className="actor-bio-card-content-label">Birthday:</span> {actor.birthday}
             </div>
             {(actor && actor.deathday && actor.deathday.length > 0)
-                ? <div className="actor-detail-card-content-container">
-                    <span className="actor-detail-card-content-label">Day of Death:</span> {actor.deathday}
+                ? <div className="actor-bio-card-content-container">
+                    <span className="actor-bio-card-content-label">Day of Death:</span> {actor.deathday}
                 </div>
-                : <div className="actor-detail-card-content-container">
-                    <span className="actor-detail-card-content-label"></span>
+                : <div className="actor-bio-card-content-container">
+                    <span className="actor-bio-card-content-label"></span>
                 </div>
             }
             <div>
-                <div className="actor-detail-card-content-container">
-                    <span className="actor-detail-card-content-label">Place of birth:</span> {actor.place_of_birth}
+                <div className="actor-bio-card-content-container">
+                    <span className="actor-bio-card-content-label">Place of birth:</span> {actor.place_of_birth}
                 </div>
-                <div className="actor-detail-card-content-container">
-                    <span className="actor-detail-card-content-label">Biography:</span><br />
+                <div className="actor-bio-card-content-container">
+                    <span className="actor-bio-card-content-label">Biography:</span><br />
                     {actor.biography && actor.biography.length > 1000 && showFullBio == true &&
-                        <span>{actor.biography} <span className="actor-detail-card-link" onClick={toggleBio}>Show less</span></span>
+                        <span className="actor-bio-card-bio">{actor.biography} <span className="actor-bio-card-link" onClick={toggleBio}>Show less</span></span>
                     }
                     {actor.biography && actor.biography.length > 1000 && showFullBio == false &&
-                        <span>{actor.biography.substring(0, 1000)}... <span className="actor-detail-card-link" onClick={toggleBio}>Show more</span></span>
+                        <span className="actor-bio-card-bio">{actor.biography.substring(0, 1000)}... <span className="actor-bio-card-link" onClick={toggleBio}>Show more</span></span>
                     }
                     {actor.biography && actor.biography.length < 1001 &&
-                        <span>{actor.biography}</span>
+                        <span className="actor-bio-card-bio">{actor.biography}</span>
                     }
-
                 </div>
             </div>
         </div>
