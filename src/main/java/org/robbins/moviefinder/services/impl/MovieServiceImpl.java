ee3ebaeb7bbace521ea5942b37192fe2bbedd7be
Justin Robbins <justinhrobbins@gmail.com>
2022-05-19 14:31:16 -0600

    fix error in retrieving watchproviders

--- src/main/java/org/robbins/moviefinder/services/impl/MovieServiceImpl.java
+++ src/main/java/org/robbins/moviefinder/services/impl/MovieServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies.MovieMethod;
 import info.movito.themoviedbapi.model.MovieDb;
+import info.movito.themoviedbapi.model.core.ResponseStatusException;
 
 @Service
 public class MovieServiceImpl implements MovieService {
@@ -19,7 +20,11 @@ public class MovieServiceImpl implements MovieService {
     @Override
     @Cacheable("moviewatchproviders")
     public MovieDb findMovieWatchProviders(int movieId, String language) {
-       return tmdbApi.getMovies().getMovie(movieId, language, MovieMethod.watch_providers);
+        try {
+            return tmdbApi.getMovies().getMovie(movieId, language, MovieMethod.watch_providers);
+        } catch (ResponseStatusException ex) {
+            return new MovieDb();   
+        }
     }
     
 }
