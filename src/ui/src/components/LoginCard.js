c658bd801b91c30d090cca99e236d116a48b041b
Justin Robbins <justinhrobbins@gmail.com>
2022-05-20 15:03:57 -0600

    Fix login timer (#80)
    * checkpoint
    
    * temporary fix

--- src/ui/src/components/LoginCard.js
+++ src/ui/src/components/LoginCard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { UserContext } from "../UserContext";
 import { AccountConfigurationCard } from './AccountConfigurationCard';
@@ -8,12 +8,17 @@ import './scss/LoginCard.scss';
 export const LoginCard = () => {
     const { loggedInUser, setLoggedInUserContext } = useContext(UserContext);
 
+    useEffect(
+        () => {
+            setTimeout(checkForValidLogin, 5000);
+        }, []
+      );
+
     const handleFailure = (failure) => {
         console.log(failure);
     };
 
     const handleLogin = async (googleData) => {
-
         const response = await fetch(process.env.REACT_APP_BACKEND_URL + 'user', {
             method: 'GET',
             headers: {
@@ -23,29 +28,38 @@ export const LoginCard = () => {
 
         const userData = await response.json();
         userData.tokenId = googleData.tokenId;
+        userData.expiresAt = googleData.tokenObj.expires_at;
         localStorage.setItem("loginData", JSON.stringify(userData));
         setLoggedInUserContext(userData);
 
         refreshTokenSetup(googleData);
     };
 
-    const handleLogout = () => {
+    const logoutAndCleanupUser = () => {
         localStorage.removeItem('loginData');
-        setLoggedInUserContext(null);
+        setLoggedInUserContext(null);        
+    }
+
+    const handleLogout = () => {
+        logoutAndCleanupUser();
     };
 
     const refreshTokenSetup = (res) => {
-        let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+        let refreshTiming = ( (res.tokenObj.expires_in || 3600) - 1800) * 1000;
+        console.log("Next refreshTiming at: " + refreshTiming);
 
         const refreshToken = async () => {
-            
+            console.log("Entering refreshToken()");
             const loginData = JSON.parse(localStorage.getItem('loginData'));
             if (!loginData) return;
 
             const newAuthRes = await res.reloadAuthResponse();
-            refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+            console.log("Updating expiresAt to " + newAuthRes.expires_at);
+            refreshTiming = ( (newAuthRes.expires_in || 3600) - 1800) * 1000;
+            console.log("Next refreshTiming in " + refreshTiming);
             
             loginData.tokenId = newAuthRes.id_token;
+            loginData.expiresAt = newAuthRes.expires_at;
             localStorage.setItem('loginData', JSON.stringify(loginData));
             setLoggedInUserContext(loginData);
 
@@ -54,6 +68,16 @@ export const LoginCard = () => {
 
         setTimeout(refreshToken, refreshTiming);
     };
+
+    const checkForValidLogin = () => {
+        console.log("Checking for valid login");
+        if ((loggedInUser) && (loggedInUser.expiresAt < Date.now())) {
+            console.log("loggedInUser: " + loggedInUser);
+            console.log("Login is expired, cleaning up");
+            logoutAndCleanupUser();
+        }
+        setTimeout(checkForValidLogin, 5000);
+    }
 
     return (
         <div className="LoginCard">
